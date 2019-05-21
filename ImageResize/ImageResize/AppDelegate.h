ca9e4a752fa9133672a938a0320ead9e6d7aa342
kuroky <kuro2007cumt@gmail.com>
2019-05-21 20:23:50 +0800

    add project

---
+++ ImageResize/ImageResize/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ImageResize
+//
+//  Created by kuroky on 2019/5/21.
+//  Copyright © 2019 Emucoo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ImageResize/ImageResize/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ImageResize
+//
+//  Created by kuroky on 2019/5/21.
+//  Copyright © 2019 Emucoo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
